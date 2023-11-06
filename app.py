9e1532d07db38cef0f9d925ac28b252e176a3a79
SharonCao0920 <ycao25sharon@gmail.com>
2023-11-05 23:01:55 -0800

    new lineup

--- app.py
+++ app.py
@@ -1,19 +1,105 @@
-from flask import Flask, render_template, request, redirect, url_for
-from utils import get_completion_from_messages
+# from flask import Flask, render_template, request, jsonify, session
+# from flask_session import Session  # Ensure Flask-Session is installed
+# import utils  # Ensure this module is available in your project
+# import openai
+# from dotenv import load_dotenv
+
+# app = Flask(__name__)
+
+# # Configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+# # Load environment variables
+# _ = load_dotenv()  # Assuming your .env file is in the root directory
+
+# # Set OpenAI API key
+# openai.api_key = utils.get_api()
+
+# # Load documents from PDF
+# path = "/home/sharoncao0802/CS589_Projects/CreateChatBot/pdfFiles/2023Catalog.pdf"  # Update the path to the actual location of your PDF
+# vectorstore = utils.db_loader(path)
+
+# # Create a QA Chain
+# qa_chain = utils.qa_Chain(vectorstore)
+
+# @app.route('/')
+# def index():
+#     if 'chat_history' not in session:
+#         session['chat_history'] = []
+#     return render_template('index.html', chat_history=session['chat_history'])
+
+# @app.route('/ask', methods=['POST'])
+# def ask():
+#     content = request.json
+#     query = content.get('question')
+
+#     if not query:
+#         return jsonify("No question provided."), 400
+
+#     chat_history = session.get('chat_history', [])
+#     result = qa_chain({"question": query, "chat_history": chat_history})
+#     chat_history.append((query, result["answer"]))
+#     session['chat_history'] = chat_history  # Store the updated history back into the session
+
+#     return jsonify(result['answer'])
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, render_template, request, jsonify, session
+from flask_session import Session  # Ensure Flask-Session is installed
+import utils  # Ensure this module is available in your project
+import openai
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    response = ""
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        # For this example, I'll use a single message from the user. 
-        # You can adjust it according to your needs.
-        messages = [{"role": "user", "content": user_input}]
-        response = get_completion_from_messages(messages)
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-    return render_template('index.html', response=response)
+# Load environment variables
+_ = load_dotenv()  # Assuming your .env file is in the root directory
+
+# Set OpenAI API key
+openai.api_key = utils.get_api()
+
+# Load documents from PDF
+path = "/home/sharoncao0802/CS589_Projects/SFBU_Main/SFBU_OpenAI_Project/pdfFiles/2023Catalog.pdf"  # Update the path to the actual location of your PDF
+vectorstore = utils.db_loader(path)
+
+# Create a QA Chain
+qa_chain = utils.qa_Chain(vectorstore)
+
+@app.route('/')
+def index():
+    # Clear the chat history on refresh
+    session.pop('chat_history', None)
+    return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    content = request.json
+    query = content.get('question')
+
+    if not query:
+        return jsonify("No question provided."), 400
+
+    chat_history = session.get('chat_history', [])
+    result = qa_chain({"question": query, "chat_history": chat_history})
+    chat_history.append((query, result["answer"]))
+    session['chat_history'] = chat_history  # Store the updated history back into the session
+
+    return jsonify(result['answer'])
+
+@app.route('/clear_history', methods=['POST'])
+def clear_history():
+    # Clear the chat history when the 'Clear History' button is pressed
+    session.pop('chat_history', None)
+    return jsonify({'status': 'history cleared'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- utils.py
+++ utils.py
@@ -1,20 +1,144 @@
 import os
+# import for pdf Loading
+from langchain.document_loaders import PyPDFLoader
+# imports for video loading
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+# import for url loading
+from langchain.document_loaders import WebBaseLoader
+# import for notion 
+from langchain.document_loaders import NotionDirectoryLoader
+# import for split PDF
+from langchain.text_splitter import CharacterTextSplitter
+#import for embedding
+from langchain.embeddings.openai import OpenAIEmbeddings
+#import for Vectorstores
+from langchain.vectorstores import Chroma
+#import for retrieve
+from langchain.llms import OpenAI
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.chains import  ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+# config api key
 from dotenv import load_dotenv, find_dotenv
-import openai
-_ = load_dotenv(find_dotenv()) # read local .env file
+llm_name = "gpt-3.5-turbo"
 
-def get_OpenAI_API_Key():
-    return os.getenv("OPENAI_API_KEY")
+def get_api():
+    _ = load_dotenv(find_dotenv()) # read local .env file
+    return os.environ['OPENAI_API_KEY']
 
-# General function to get response with gpt-3.5
-def get_completion_from_messages(messages, 
-                                 model="gpt-3.5-turbo", 
-                                 temperature=0, 
-                                 max_tokens=1000):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, 
-        max_tokens=max_tokens, 
+
+
+# Function to load PDF file
+# pip install pypdf 
+def loadPDF(path):
+    loader = PyPDFLoader(path)
+    pages = loader.load()
+    return pages
+
+
+# Function to load video
+# pip install yt_dlp
+# pip install pydub
+def loadVideo(url):
+    save_dir="docs/youtube/"
+    loader = GenericLoader(
+        YoutubeAudioLoader([url],save_dir),
+        OpenAIWhisperParser()
     )
-    return response.choices[0].message["content"]
+    docs = loader.load()
+    return docs
+
+# function to load url
+def loadURL(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    return docs
+
+# function to load notion
+def loadNotion(path):
+    loader = NotionDirectoryLoader(path)
+    docs = loader.load()
+    return docs
+
+#Split Documents
+def splitCharacterText(pages):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    return text_splitter.split_documents(pages)
+
+# Embedding
+def embeddingText():
+    return OpenAIEmbeddings()
+
+#In vectordtores
+def saveVectorStores(splits):
+    vectordb = Chroma.from_documents(
+        documents=splits,
+        embedding=embeddingText(),
+    )
+    return vectordb
+
+#Search similarity
+def similaritySearch(vectordb, question, k):
+    docs = vectordb.similarity_search(query=question, k=k)
+    return docs
+
+# def pretty_print_docs(docs):
+#     response = "\n{'-' * 100}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)])
+#     return response
+
+# Retrieve
+def retrieve(vectordb, question):
+    # Wrap our vectorstore
+    llm = OpenAI(temperature=0)
+    compressor = LLMChainExtractor.from_llm(llm)
+    compression_retriever = ContextualCompressionRetriever(
+            base_compressor=compressor,
+            base_retriever=vectordb.as_retriever(search_type = "mmr")
+    )
+    compressed_docs = compression_retriever.get_relevant_documents(question)
+    return compressed_docs
+
+def answerWithBuildPrompt(vectordb, template, question):
+    llm=ChatOpenAI(model_name=llm_name, temperature=0)
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+    qa = ConversationalRetrievalChain.from_llm(llm,
+                                       retriever=vectordb.as_retriever(),
+                                       return_source_documents=True,
+                                       memory=memory,
+                                       chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+    result = qa({"question": question})
+    return result['answer']
+
+def db_loader(pdfPath):
+    pages = loadPDF(pdfPath)
+
+    #split document
+    docs = splitCharacterText(pages)
+
+    # indexing and Save in vectorstores
+    vectordb = saveVectorStores(docs)
+    return vectordb
+
+def qa_Chain(vectorstore):
+    return ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectorstore.as_retriever())
+    
+
+
